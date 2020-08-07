c44859929a96d01b51ad7316172230a27cc5c0c8
Jaid <jaid.jsx@gmail.com>
2020-08-07 02:58:36 +0200

    Patch
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- index.user.js
+++ index.user.js
@@ -19,12 +19,10 @@
   .tw-c-background-alt,
   .chat-shell .chat-room,
   .mosaic-window-body,
-  .side-nav__overlay-wrapper
-  {
-    background: black !important;
-  }
+  .side-nav__overlay-wrapper,
   .tw-input,
-  textarea[data-test-selector="chat-input"]
+  textarea[data-test-selector="chat-input"],
+  .tw-core-button--secondary
   {
     background: black !important;
   }
@@ -32,18 +30,65 @@
     background: #0b0b0b !important;
     color: #828282 !important;
   }
-  .tw-core-button--secondary {
-    background: black !important;
-    border: 1px solid transparent;
-  }
   .tw-core-button--secondary:hover {
-    border: 1px solid #333;
+    background: var(--color-background-interactable-alpha-hover) !important;
   }
   .tw-root--theme-dark {
     --color-background-alt: black;
     --color-background-alt-2: black;
     --color-background-base: black;
     --color-background-body: black;
+    --color-background-input: black;
+    --color-fill-brand: #3a0d7e;
+    --color-background-interactable-alpha-hover: #161616;
+    --brand-border: #9147ff; /* not from twitch, selfmade var */
+  }
+  .top-nav {
+    border-bottom: var(--border-width-default) solid var(--color-border-base);
+  }
+  #sideNav {
+    border-right: var(--border-width-default) solid var(--color-border-base) !important;
+  }
+  .sunlight-top-nav {
+    border-bottom: var(--border-width-default) solid var(--color-border-base);
+  }
+  .sunlight-nav-drop-down-menu {
+      border-right: var(--border-width-default) solid var(--color-border-base);
+  }
+  .tw-flex[data-a-target="chat-viewers"] {
+    background: none !important;
+  }
+
+  /* https://www.twitch.tv/directory/esports */
+
+  .esports-category-selector-card__background {
+    background: none !important;
+  }
+
+  /* https://dashboard.twitch.tv/u/jaidchen/content/video-producer */
+
+  .video-manager-processed-list .video-card {
+    background: black;
+    border: var(--border-width-default) solid var(--color-border-base) !important;
+  }
+  .video-manager-processed-list .video-card:hover {
+    background: black;
+    border: var(--border-width-default) solid var(--brand-border) !important;
+  }
+  .video-manager-processed-list .video-card>div>a:hover {
+    background: none !important;
+  }
+
+  /* https://dashboard.twitch.tv/u/jaidchen/content/clips/channel */
+
+  .clmgr-table__row {
+    background: none !important;
+  }
+  .clmgr-table__row:hover {
+    outline: var(--border-width-default) solid var(--brand-border) !important;
+  }
+  .clmgr-table__row>button:hover {
+    background: none !important;
   }
   `;
   const element = document.createElement("style");
